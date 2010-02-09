407173573edb3873835ef4956a10ba8097c87462
Thomas Wirum Larsen <thomas.wirum@gmail.com>
2010-02-09 04:49:23 +0100

    Halfway done with the last method of TekstBehandling.

--- src/oppgavesett4/Oppgave3.java
+++ src/oppgavesett4/Oppgave3.java
@@ -2,8 +2,8 @@ package oppgavesett4;
 
 public class Oppgave3 {
 	public static void main(String[] args){
-		TekstBehandling tekst = new TekstBehandling("Dette er en test lol, med forskjellige tegn. Og bokstaver!");
-		System.out.println(tekst.antallOrd());
-		System.out.println(tekst.ordLengde());
+		TekstBehandling tekst = new TekstBehandling("Dette er en test lol, med forskjellige tegn. Og bokstaver! Og spørsmål?");
+
+		System.out.println(tekst.perPeriode());
 	}
 }
--- src/oppgavesett4/TekstBehandling.java
+++ src/oppgavesett4/TekstBehandling.java
@@ -1,5 +1,8 @@
 package oppgavesett4;
 
+import java.awt.image.ReplicateScaleFilter;
+import java.util.HashMap;
+
 // Dette er oppgave 5
 public class TekstBehandling {
 	private String tekst;
@@ -9,6 +12,9 @@ public class TekstBehandling {
 	}
 	public String hentTekst(){
 		return tekst;
+	}
+	public String hentUcase(){
+		return tekst.toUpperCase();
 	}
 	public int antallOrd(){
 		String[] antall = tekst.split(" ");
@@ -21,5 +27,15 @@ public class TekstBehandling {
 			lengde += i.length();
 		}
 		return (lengde / antall.length);
+	}
+	public String perPeriode(){
+		StringBuilder builder = new StringBuilder();
+		String strengMedHashes = "";
+		strengMedHashes = tekst .replaceAll("\\!","#")
+								.replaceAll("\\?","#")
+								.replaceAll("\\,","#")
+								.replaceAll("\\.","#");
+		String[] periodeArray = strengMedHashes.split("# ");
+		return "temp";
 	}
 }
